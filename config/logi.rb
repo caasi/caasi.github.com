072c2c2797f795500833b0869b1f866f048af8b9
caasi Huang <caasi.igd@gmail.com>
2013-09-20 03:53:56 +0800

    use @godfat's site generator "logi"

---
+++ config/logi.rb
@@ -0,0 +1,9 @@
+
+post_path   'post'
+layout_path 'layout'
+output_path 'public'
+
+layout 'default.html.erb'
+
+post 'hello.md', :layout => 'another.html.erb'
+list 'list'    , :layout => 'list.html.erb'
